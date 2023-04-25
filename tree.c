07147427ae534e26b0d614a634f16347acdc0a6a
Callum Beaney <79003912+CallumBeaney@users.noreply.github.com>
2023-04-25 16:05:10 +0100

    Update tree.c

--- tree.c
+++ tree.c
@@ -1,40 +1,68 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
 
-int main(void) {
-  int a;
-  do {
-    printf("How high between 5 & 15? ");
-    scanf("%i", & a);
-  } while (a < 5 || a > 15);
-  for (int b = 0; b <= a; b++) {
-    printf(" ");
-  }
-  printf("##");
-  for (int c = 0; c <= a; c++) {
-    printf(" ");
-  }
-  printf("\n");
-  for (int x = 0; x < a; x++) {
-    for (int y = 0; y < a - x; y++) {
-      printf(" ");
+int main(void)
+{
+    system("clear");
+
+    int a = 8;
+
+    for (int pp = 0; pp < 10; pp++)
+    {
+        system("clear");
+
+        printf("\n\n");
+        for (int b = 0; b <= a; b++)
+        {
+            printf(" ");
+        }
+        printf("##");
+
+        for (int c = 0; c <= a; c++)
+        {
+            printf(" ");
+        }
+        printf("\n");
+
+        for (int x = 0; x < a; x++)
+        {
+            for (int y = 0; y < a - x; y++)
+            {
+                printf(" ");
+            }
+            for (int z = 0; z <= x; z++)
+            {
+                printf("#");
+            }
+            printf("||");
+            for (int z = 0; z <= x; z++)
+            {
+                printf("#");
+            }
+            for (int y = 0; y < a - x; y++)
+            {
+                printf(" ");
+            }
+            printf("\n");
+        }
+
+        for (int j = 0; j <= a; j++)
+        {
+            printf("_");
+        }
+        printf("||_田");
+        for (int k = 0; k <= a - 3; k++)
+        {
+            printf("_");
+        }
+        printf("stage: %d", pp);
+
+        fflush(stdout);
+        sleep(1); 
+    
     }
-    for (int z = 0; z <= x; z++) {
-      printf("#");
-    }
-    printf("||");
-    for (int z = 0; z <= x; z++) {
-      printf("#");
-    }
-    for (int y = 0; y < a - x; y++) {
-      printf(" ");
-    }
-    printf("\n");
-  }
-  for (int j = 0; j <= a; j++) {
-    printf("_");
-  }
-  printf("||_田");
-  for (int k = 0; k <= a - 3; k++) {
-    printf("_");
-  }
 }
