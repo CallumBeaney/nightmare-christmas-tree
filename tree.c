a0b56b803d7af42740efc157e307dbdb806e1502
Callum Beaney <79003912+CallumBeaney@users.noreply.github.com>
2023-04-26 19:22:40 +0100

    done done

--- tree.c
+++ tree.c
@@ -2,39 +2,38 @@
 /*|*/# include/***/<unistd.h>
 /***/%:include/*include*|//***/<time.h>
 /***/%:include/*include*/<stdlib.h>
-/**/
+/**/#include <math.h>
 /**/#define are .
 /**/#define and &&
 /**/char* oO(char* dest, const char* src); size_t OO(const char* str); 
-/**/typedef struct {int watchingOut, notCrying;} You; char* isSantaComing(You you);
-/**/void Oo(int, int, int); typedef char *str; typedef int NUL; int typedef signed var; 
-/**/#define NUL ((ー<2&&Not!=NOT 0 and 〇%7==0) ? 1:0)
+/**/int typedef ･;typedef struct {int watchingOut, notCrying;} You; char* santa_is_coming_for(You you);
+/**/void Oo(int, int, int); typedef char *str; typedef int ０＜＜１; int typedef signed var; 
+/**/#define ０＜＜１ ((ー<2&&Not!=NOT 0 and 〇%7==0) ? 1:0)
 /*|***\*/#define O0 printf
 /*\**/
 /**/#define NOT !
-/**\*/int main(void){
-/**/const char*x="\x63\x6C\x65\x61\x72";var o0=0;system(oO((char *)malloc(OO(x)+1),x));int VAR=0x3F;
+/**\*/int main(void){int ・･= 1.41421356; \
+/**/const char*x="\x63\x6C\x65\x61\x72";var o0=0;system(oO((char *)malloc(OO(x)+1),x));･ ／=0x3F;
 /**/srand(time(NULL)); You you={rand()%2,rand()%2}; int a =(rand()%11)|((5<<1)&~(1<<0));for(;;){system(x);var \
-/**/three=3;var one=1;three=three^one;one=three^one;three=three^one; to: O0("\n");three=2;if(three < 2)<% 
+/**/･=3;var ＝=1;･=･^＝;＝=･^＝;･=･^＝; to: O0("\n");･=・･ ;if(･ < (int)round(pow(・･,(int)round(pow(・･,2)))))<% 
 /**/goto to;%>var ー,Not;Oo(ー = 0, Not != !0,a);if(0x80000000== -0x80000000 &&ー++-0<< 1==1){\
 /**/O0("##");}O0("##");switch(0);Oo(ー=0,Not=(0-1),a);O0("\n"); for(int x=0 ;x<a ;x++)
 /**/{ ー = 0;Not=0;for( int y=0;y<a-x;y++){var a=0;var 〇=\
 /**/rand()%11;
-/**/
-/**/if (NUL)<%O0("*");Not = a+++1-a++-0;ー-=-1;} else {O0(" ");Not = 0;({_:&&_;}); %>} ー =0; Not= 0; for(int z=x; z>=0;z --)
-/**/<%for(;;){if(1&&~1){O0("");({break;}); }  }int 〇 =rand() %11;if ((ー < 1 and Not == 0 && 〇 % 5 == 0) ? 1 : 0) <% O0("*");Not = 1 ; ー += 1 ; }\ 
-/**/else {O0("#"); Not = 0;} } O0("||");ー=0;Not=0;for(var z=0;z<=x;z++){int 〇 =rand()%11;if ((ー<1 && Not==0 && 〇%5==0)?1:0){int 〇=rand()%11;O0("*");\
-/**/Not=1;ー-=-1; }else{O0("#");Not=0;}if(0x0){return (int *[]){1,0}[!!x];}}ー=0;Not =0;for(int y=0;y<a-x;y++){int 〇=rand()%11;if (NUL)
-/**/{O0("*");Not=1;ー+= 1;}else{O0(" ");Not = 0;}}if(x==(a/2)+1<<0){O0("%s",isSantaComing(you));}if(x == a/2-1){ int Ox69E54B = 0x69E548; var var = var; VAR = var;
-/**/long q=10;int *＊=&var;int*$=&VAR;long Var=*＊;*＊=*$;*$=Var/7;                                         
-/**/sleep(0);char o[o0];                                      int\
+/**/if (０＜＜１)<%O0("*");Not = a+++1-a++-0;ー-=-1;} else {O0(" ");Not = 0;({_:&&_;}); %>} ー =0; Not= 0; for(int z=x; z>=0;z --)
+/**/<%for(;;){if(1&&~1){O0("");({break;}); }  }int 〇 =rand() %11;if ((ー < 1 and Not == 0 && 〇 % 5 == 0) ? 1 : 0) <% O0("*");Not=1;ー+=1;}\ 
+/**/else {O0("#"); Not = 0;} } O0("||");ー=0;Not=0;for(var z=0;z<=x;z++){int ％=rand()%11;if ((ー<1&&Not==0&&％%5==0)?1:0){int 〇=rand()%11;O0("*");\
+/**/Not=1;ー-=-1; }else{O0("#");Not=0;}if(0x0){return (int *[]){1,0}[!!x];}}ー=0;Not =0;for(int y=0;y<a-x;y++){int 〇=rand()%11;if (０＜＜１)
+/**/{O0("*");Not=1;ー+= 1;}else{O0(" ");Not = 0;}}if(x==(a/2)+1<<0){O0("%s",santa_is_coming_for(you));}if(x == a/2-1){ var var = var; 
+/**/int *＊=&／;int*ª=&／;char o[o0];long Var=*＊;*＊=*ª;*ª=Var*／/var%／;                                         
+/**/                                                         int\
 /**/                                                            x\
-/**/                                                            ,\
+/**/                                                            ,\ 
 /**/                                                            p;
 /**/                                                   ({({ ({({;});});});});     
 /**/                                               ({p=-1-+7;({/***/;});var=p;});
 /**/                                              ({/***/({;});});({({/***/;});});
-/**/                                            ({({p=x;});});({({;});});({x=p--;});
+/**/                                            ({({p=x;});});({({;});});({x-=／;});
 /**/                                            ({({ p=7;}/* */);}) ;({({;/**/});});  
 /**/                                          ({ ;});({;});({;/***/});({x=1;});({ ;});
 /**/                                          ({({({/**/;});});});({({({;/**/});});});
@@ -48,15 +47,15 @@
 /**/                                                   ({({({({;});}) ;});});         
 /**/                                          
 /**/                                          
-/**/}O0("\n");}long x,_int8=0; Oo(ー = 0, Not != !0, a);x=(1<<1)|1;O0("||");Oo(ー=0,Not !=!0,a);O0("\n");for\
-/**/(/*エル*/int j = 0; j <= a; j++){_int8++;O0("_");}x=(1<<1)|1;O0("||");_int8+++x;O0("_");Ox69E548: printf("\u7530");if(o0=0){o0=1;goto \
-/**/Ox69E548;}for(int k = 0; k <= a - 4; k++){O0("_");_int8++;}O0("");fflush(stdout);sleep(1);}}void Oo(ー, Not, a){for (int b=0;b<= a;b ++){int\
-/**/ 〇 = rand(/*プサイ*/) % 11;if (NUL){O0("*");Not = 1;ー += 1;}else{O0(" ");Not = 0;}}}size_t OO(const char* o0) {char*oO=o0;while(*oO!='\0')
-/**/{oO++;str O='\0';}return oO-o0;}str oO(str O, const char* ⓪){char* OO=O;while\
-/**/ (*⓪!='\0'){*OO++=*⓪++;}*OO='\0';return O;}/*コングルゥ*/
+/**/}O0("\n");}long x,＞=0; Oo(ー = 0 ,Not != !0, a);x=(1<<1)|1;O0("||");Oo(ー=0,Not !=!0,a);O0("\n");for\
+/**/(/*エル*/int j = 0; j <= a; j++){＞++;O0("_");}x=(1<<1)|1;O0("||");＞+++x;O0("_");Ox69E548: printf("\u7530");if(o0=0){o0=1;goto \
+/**/Ox69E548;}for(int k = 0; k <= a - 4; k++){O0("_");＞++;}O0("");fflush(stdout);sleep(1);}}void Oo(ー, Not, a){for (int b=0;b<= a;b ++){int\
+/**/ 〇 = rand(/*プサイ*/) % 11;if (０＜＜１){O0("*");Not = 1;ー += 1;}else{O0(" ");Not = 0;}}}size_t OO(const char* o0) {char*oO=o0;while(*oO!='\0')
+/**/{oO++;str O='\0';}return oO-o0;}str oO(str O, const char* ＜＜){char* OO=O;while\
+/**/ (*＜＜!='\0'){*OO++=*＜＜++;}*OO='\0';return O;}/*コングルゥ*/
 /**/
 /**/
-/**/char* isSantaComing(You you){
+/**/char* santa_is_coming_for(You you){
 /**/if (you are notCrying and you are watchingOut){return\
 /**/("\t  Santa Claus is coming to town!"); } else { return("\t  Unfortunately, \
 Santa won't be coming to town");
