ef2cf0a18ad7c5e8efea460a1d869a79113f0ad0
Callum Beaney <79003912+CallumBeaney@users.noreply.github.com>
2023-04-26 03:59:27 +0100

    upload

---
+++ README.md
@@ -0,0 +1,4 @@
+Generates a tree of a random height and generates little snowflakes. Also the code is obfuscated.  
+
+<img src="./pic.png" alt="Image description" style="width: 400px;">
+
--- tree.c
+++ tree.c
@@ -1,68 +1,43 @@
 #include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
+%:include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <time.h>
+# include <unistd.h>
+%:include <time.h>
 
-int main(void)
-{
-    system("clear");
 
-    int a = 8;
+#define NOT !
+int typedef signed var;
+typedef int NUL;
+#define P printf
+typedef char *string;
+void top(int, int, var);
+#define NUL ((ー<2 && Not != NOT 0 && 〇 % 7 == 0) ? 1 : 0)
 
-    for (int pp = 0; pp < 10; pp++)
-    {
-        system("clear");
 
-        printf("\n\n");
-        for (int b = 0; b <= a; b++)
-        {
-            printf(" ");
-        }
-        printf("##");
+int main(void) { const char *x = "\x63\x6C\x65\x61\x72"; system(strcpy((char *)malloc(strlen(x) + 1),x)); srand(time(NULL)); int a =(rand()%11)|((5<<1)&~(1<<0));
 
-        for (int c = 0; c <= a; c++)
-        {
-            printf(" ");
-        }
-        printf("\n");
+  
+for (;;) /* here */ { system(x);var three=3;var one=1;three=three^one;one=three^one;three=three^one; to: P("\n");three=2; if (three < 2)<%goto to; %> int ー,Not;top(ー = 0, Not != !0, a); if(0x80000000 == -0x80000000 && ー++-0<<1 == 1){P("##");}P("##");switch(0);top(ー=0,Not=(0-1),a);P("\n"); for(int x=0;x<a;x++){ ー = 0;Not=0;for( int y=0;y<a-x;y++){var a = 0;var 〇=rand()%11;if (NUL)<%P("*");Not = a+++1-a++-0;ー-=-1;} else {P(" ");Not = 0;({_:&&_;}); %>} ー =0; Not= 0; for( int z = x; z >= 0; z--) <%for(;;){if(1&&~1){P("");({break;}); }  }int 〇 =rand() %11;if ((ー < 1 && Not == 0 && 〇 % 5 == 0) ? 1 : 0) <% P("*");Not = 1 ; ー += 1 ; } else {P("#"); Not = 0;} } P("||");ー=0;Not=0;for(int z=0;z<=x;z++){int 〇 =rand()%11;if ((ー<1 && Not==0 && 〇%5==0)?1:0){int 〇=rand()%11;P("*");Not=1;ー-=-1; }else{P("#");Not=0;}if(0x0){return (int *[]){1,0}[!!x];}}ー=0;Not =0;for(int y=0; y<a-x;y++){int 〇=rand()%11;if (NUL){P("*");Not=1;ー+= 1;}else{P(" ");Not = 0;}}if(x == a/2)                               {                            
+                              int\ 
+                                x\
+                                 ,\
+                                 p\
+                                 ;
+                       ({({({({ ;});}); });});
+                     ({p =1;({/***/;});x=p+4;});
+                   ({/***/({;});});({({/**/;});});
+                 ({({ ;});});({({;});});({x=p--;});
+                ({({ p=7;}/* */);}) ;({({;/* */});});
+              ({ ;});({;});({;/***/});({x=1;});({ ;});
+              ({({({/**/;});});});({({({;/**/});});});
+              P("     ｍｅｒｒｙ  ｃｈｒｉｓｔｍａｓ    ");   
+              ({({({/**/;});});});({({({;/**/});});});
+              ({p= 11;});({;});({;/**/});({ ;});({;});
+               ({ ({ ;}/**/);});({({x=NULL;/**/});});
+                ({({x=3;});})/***/;({({ ;});});p=77;
+                  ({/***/({ ;});});({({;})/***/;});
+                    ({({;});({p=7;}/***/); x=p;});
+                      ({(/**/{({({;});});});});
 
-        for (int x = 0; x < a; x++)
-        {
-            for (int y = 0; y < a - x; y++)
-            {
-                printf(" ");
-            }
-            for (int z = 0; z <= x; z++)
-            {
-                printf("#");
-            }
-            printf("||");
-            for (int z = 0; z <= x; z++)
-            {
-                printf("#");
-            }
-            for (int y = 0; y < a - x; y++)
-            {
-                printf(" ");
-            }
-            printf("\n");
-        }
 
-        for (int j = 0; j <= a; j++)
-        {
-            printf("_");
-        }
-        printf("||_田");
-        for (int k = 0; k <= a - 3; k++)
-        {
-            printf("_");
-        }
-        printf("stage: %d", pp);
-
-        fflush(stdout);
-        sleep(1); 
-    
-    }
-}
+} P("\n");    }long x,_int8=0; for (int j = 0; j <= a; j++)    {_int8++;P("_");}x = (1 << 1) | 1;P("||_田");_int8+++x;for (int k = 0; k <= a - 3; k++){P("_");_int8++;}fflush(stdout);sleep(1);}}void top(ー, Not, a){for (int b = 0; b <= a; b++){int 〇 = rand() % 11;if (NUL){P("*");Not = 1;ー += 1;}else{P(" ");Not = 0;}}}
