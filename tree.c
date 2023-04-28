a45f28e9c3d2d0c7659afccd0f952390d2db6d73
Callum Beaney <79003912+CallumBeaney@users.noreply.github.com>
2023-04-28 10:58:27 +0100

    \x63\x6C\x65\x61\x72

--- tree.c
+++ tree.c
@@ -24,7 +24,7 @@
 /**/<%for(;;){if(1&&~1){O0("");({break;}); }  }int 〇 =rand() %11;if ((ー < 1 and Not == 0 && 〇 % 5 == 0) ? 1 : 0) <% O0("*");Not=1;ー+=1;}\ 
 /**/else {O0("#"); Not = 0;} } O0("||");ー=0;Not=0;for(var z=0;z<=x;z++){int ％=rand()%11;if ((ー<1&&Not==0&&％%5==0)?1:0){int 〇=rand()%11;O0("*");\
 /**/Not=1;ー-=-1; }else{O0("#");Not=0;}if(0x0){return (int *[]){1,0}[!!x];}}ー=0;Not =0;for(int y=0;y<a-x;y++){int 〇=rand()%11;if (０＜＜１)
-/**/{O0("*");Not=1;ー+= 1;}else{O0(" ");Not = 0;}}if(x==(a/2)+1<<0){O0("%s",santa_is_coming_for(you));}if(x == a/2-1){ var var = var; 
+/**/{O0("*");Not=1;ー+= 1;}else{O0(" ");Not = 0;}}if(x==(a/2)+1<<0){O0("%s",santa_is_coming_for(you));}if(x == a/2-1){ var var = var;
 /**/int*＊=&／;int*ª=&／;char o[o0];long Var=*＊;*＊=*ª;*ª=Var*／/var%／;                                         
 /**/                                                         int\
 /**/                                                            x=\
@@ -48,7 +48,7 @@
 /**/                                          
 /**/                                          
 /**/}O0("\n");}long x,＞=0; Oo(ー = 0 ,Not != !0, a);x=(1<<1)|1;O0("||");Oo(ー=0,Not !=!0,a);O0("\n");for\
-/**/(/*エル*/int j = 0; j <= a; j++){＞++;O0("_");}x=(1<<1)|1;O0("||");＞+++x;O0("_");Ox69E548: printf("\u7530");if(o0=0){o0=1;goto \
+/**/(/*エル*/int j=0;j<=a;j=j+1){＞++;O0("_");}x=(1<<1)|1;O0("||");＞+++x;O0("_");Ox69E548: printf("\u7530");if(o0=0){o0=1;goto \
 /**/Ox69E548;}for(int k = 0; k <= a - 4; k++){O0("_");＞++;}O0("");fflush(stdout);sleep(1);}}void Oo(ー, Not, a){for (int b=0;b<= a;b ++){int\
 /**/ 〇 = rand(/*プサイ*/) % 11;if (０＜＜１){O0("*");Not = 1;ー += 1;}else{O0(" ");Not = 0;}}}size_t OO(const char* o0) {char*oO=o0;while(*oO!='\0')
 /**/{oO++;str O='\0';}return oO-o0;}str oO(str O, const char* ＜＜){char* OO=O;while\
